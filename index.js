c2b6db75e857b6cbb4e52c21917a504316702ecd
Shawn <iamshawnhartline@gmail.com>
2025-01-23 20:09:50 -0600

    exercise 3 done

---
+++ index.js
@@ -0,0 +1,46 @@
+// Exercise 1:
+const car = {
+	make: "Ford",
+	model: "Ranger",
+	year: "1995",
+	getInfo: function () {
+		console.log(`The car is a ${this.year} ${this.make} ${this.model}`);
+	},
+};
+
+car.getInfo();
+
+//Exercise 2:
+const student = {
+	name: "Shawn",
+	age: "39",
+	grades: [99, 100, 80, 92, 75],
+	calculateAverageGrade: function () {
+		let total = 0;
+
+		for (index = 0; index < this.grades.length; index++) {
+			let currentIndex = grades[index];
+			total += currentIndex;
+		}
+	},
+};
+
+//Exercise 3
+const btn = document.getElementById("btn");
+const bgColor = document.getElementById("bgColor");
+let i = 1;
+btn.addEventListener("click", function () {
+	if (i === 0) {
+		bgColor.style.backgroundColor = "white";
+		i++;
+	} else if (i === 1) {
+		bgColor.style.backgroundColor = "red";
+		i++;
+	} else if (i === 2) {
+		bgColor.style.backgroundColor = "blue";
+		i++;
+	} else {
+		bgColor.style.backgroundColor = "yellow";
+		i = 0;
+	}
+});
